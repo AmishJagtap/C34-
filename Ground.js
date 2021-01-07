f2e3fc7c965021a72724a571543fbc471bff0a59
AmishJagtap <amishjagtap@gmail.com>
2021-01-07 13:04:40 +0530

    amish

---
+++ Ground.js
@@ -0,0 +1,16 @@
+class Ground{
+
+constructor(x,y,width,height){
+
+this.body = Bodies.rectangle(x,y,width,height,options);
+this.width = width
+
+}
+
+
+
+
+
+
+
+}
--- sketch.js
+++ sketch.js
@@ -1,9 +1,41 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine,world;
+var ball,box1,box2,box3
+
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(3000,800);
+engine = Engine.create();
+ world = engine.world;
+
+
+ground = new Ground(1500,2900,3000,50);
+ball = new Ball(200,200,80,80);
+box1 = new box(900,100,70,70);
+//box1 to box6 = x = 900
+//box7 to box12 = x = 800
+//box 13 to box 20 = x= 700
 }
 
 function draw() {
-  background(255,255,255);  
+  background(255,255,255);
+  Engine.update(engine);
+  ground.display();
+  box1.display();
+
+
+
+
+
+  ball.display();
+  rope.display();
   drawSprites();
+}
+function mouseDragged(){
+
+
 }
